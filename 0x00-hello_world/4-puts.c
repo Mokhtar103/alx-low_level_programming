4cdded3da810764cc9ee09d5d6acb0ae47ded2cf
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-16 07:28:35 -0700

    using puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * puts - ouput characters of string with a newline 
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
