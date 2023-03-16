4aa29a31ca60ea2bff8d12fa2b09b7747ac473eb
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-16 07:41:33 -0700

    using printf func

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - A program thats print a line with printf function
  *
- * Return : Always 0(Success)
+ * Return : 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
