e34e14a8fc48e390d494e9486f8942ca32b770f4
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-17 14:17:49 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - a program which print the numbers
+* Return: 0(Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d", i);
+}
+	putchar('\n');
+	return (0);
+}
+
