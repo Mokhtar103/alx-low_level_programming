68acd9a0cc768b2a93040a767e6122a2c7a34183
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-17 14:59:02 -0700

    print hexa numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - a program which print the numbers
+* Return: 0(Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 47; ch < 103; ch++) {
+		
+		if (ch >= 48 && ch <= 57)
+		{
+			putchar(ch);
+		}
+		else if (ch >= 97 && ch <= 102)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
