b585122f796380fbbd0e58b54fbe7a389600863a
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-17 14:34:22 -0700

    print number using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - a program which print the numbers
+* Return: 0(Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
