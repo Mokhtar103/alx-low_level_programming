a10fc1c15671bf66b1096791b350a173837fae7c
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-17 14:07:30 -0700

    alphabet in lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - a program which print the alphabet in lowercase
+* Return: 0(Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
