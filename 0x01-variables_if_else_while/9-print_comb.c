878e1ffe1fbba6680372657c1f4f29c3d2ed9bd6
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-17 15:29:26 -0700

    print numbers with commas and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,17 @@
 */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 48; i <= 57; i++)
+for (i = 48; i <= 57; i++)
+{
+	putchar(i);
+	if (i != 57)
 	{
-		putchar(i);
-		if (i != 57)
-		{
 		putchar(',');
 		putchar(' ');
-		}
-    }
-	putchar('\n');
-	return (0);
+	}
+}
+putchar('\n');
+return (0);
 }
