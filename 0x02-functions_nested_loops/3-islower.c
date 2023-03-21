1daeb7ba63d7c677c004c68f56eb3ddd5dcf1be0
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-21 08:32:11 -0700

    check for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 /**
 * _islower - checks for lowercase character
 * Return: int
+* c : character that the function will convert to asci num
 */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
