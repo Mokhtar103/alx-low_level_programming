97a4ef641f1d604203c7e0c3b9a72cfd7572fd97
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-23 15:15:14 -0700

    multi of two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiply two digits
+* @a : the first digit to be multiplied
+* @b : the second digit to be multiplied
+* Return: the result of multiplication
+*/
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+return (result);
+}
