3ac3e24c37fcbfb2bd985332a761ed0024cdb1ff
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-03-30 17:54:31 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - convert lowercase to uppercase
+ * @str: string to be converted
+ * Return: uppercase string
+ */
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+}
