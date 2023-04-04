4fab38c0a9bdc20a12d87e6ad3d5842f53638874
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-03 18:14:36 -0700

    print sum of diagnoals of square

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagsums - print the sum of diagonals
+ * @a : array of integers
+ * @size : size of the array
+ * Return: empty
+ */
+void print_diagsums(int *a, int size)
+{
+int i, sum1, sum2;
+sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i];
+sum2 += a[size - i - 1];
+a += size;
+}
+printf("%d, %d\n", sum1, sum2);
+}
