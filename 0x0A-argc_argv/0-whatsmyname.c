136aeb28d061fc99a0a2721b40a44f6c306beae0
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-07 12:59:01 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints its name
+ * @argc: arguments count
+ * @argv: array that conatin the arguments
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
