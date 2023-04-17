c33ff058bb3ed5763b58da3f76edb94652292397
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-17 13:36:47 -0700

    prints dog data

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,12 +5,33 @@
  * @d: structure
  * Return: empty
  */
-void print_dog(struct dog *d)
+void print_dog(struct dog* d)
 {
 if (d != NULL)
 {
-printf("Name: %s\n", d->name ? d->name : "(nill)");
+if (d->name == NULL)
+{
+printf("Name: (nill)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+if (d->age < 0)
+{
+printf("Age: (nill)\n");
+}
+else
+{
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+}
+if (d->owner == NULL)
+{
+printf("Owner: (nill)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
 }
 }
