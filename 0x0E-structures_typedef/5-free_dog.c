3edce4020365ca1a577bc4411523356f49176e88
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-17 15:29:13 -0700

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  free dogs
+ * @d: dog
+ * Return: empty
+ */
+void free_dog(dog_t *d)
+{
+if (d->name != NULL)
+{
+free(d->name);
+}
+if (d->owner != NULL)
+{
+free(d->owner);
+}
+free(d);
+}
