8fcdbba5795c39702ad99695378c8140fe5ad2e7
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-19 18:31:24 -0700

    execute a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * array_iterator - executes a function
@@ -8,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int *last = &array + size - 1;
+int *last = array + size - 1;
 
 if (size && array && action)
 {
