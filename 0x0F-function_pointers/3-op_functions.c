ac92d3d45a1b49ea414c97f12ae1862d4c418c3e
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-19 19:06:27 -0700

    functions of ops

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - return the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: differece
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - return the result of div of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainder of div of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
