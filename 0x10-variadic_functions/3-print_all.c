0d98867513f8831dde80299f149ed9a64fc21fbf
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2023-04-20 17:55:59 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-/**
- * char_format - character format
- * @separator: separator between strings
- * @all : argument pointer
- * Return: empty
- */
-void char_format(char *separator, va_list all)
-{
-printf("%s%c", separator, va_arg(all, int));
-}
-
-/**
- * float_format - float format
- * @separator: separator between strings
- * @all : argument pointer
- * Return: empty
- */
-void float_format(char *separator, va_list all)
-{
-printf("%s%f", separator, va_arg(all, int));
-}
-
-/**
- * string_format - string format
- * @separator: separator between strings
- * @all : argument pointer
- * Return: empty
- */
-void string_format(char *separator, va_list all)
-{
-char *str = va_arg(all, char *);
-
-if (str == NULL)
-{
-printf("(nil)");
-}
-else
-{
-printf("%s%s", separator, str);
-}
-}
-
-/**
- * int_format -  integer format
- * @separator: separator between strings
- * @all : argument pointer
- * Return: empty
- */
-void int_format(char *separator, va_list all)
-{
-printf("%s%d", separator, va_arg(all, int));
-}
 
 /**
  * print_all -  print anything
@@ -61,32 +9,43 @@ printf("%s%d", separator, va_arg(all, int));
  */
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *separator = "";
+int i = 0;
+char *str,  *separator = "";
+
 va_list all;
-token tokens[] = {
-{"c", char_format},
-{"i", int_format},
-{"f", float_format},
-{"s", string_format},
-{NULL, NULL}
-};
+
 va_start(all, format);
-i = 0;
-while (format && format[i])
+
+if (format)
 {
-j = 0;
-while (tokens[j].token)
+while (format[i])
 {
-if (format[i] == tokens[j].token[0])
+switch (format[i])
 {
-tokens[j].f(separator, all);
+case 'c':
+printf("%s%c", separator, va_arg(all, int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(all, int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(all, double));
+break;
+case 's':
+str = va_arg(all, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", separator, str);
+break;
+default:
+i++;
+continue;
+}
 separator = ", ";
-}
-j++;
-}
 i++;
 }
+}
+
 printf("\n");
 va_end(all);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdarg.h>
-#include <stdio.h>
-/**
- * struct myToken -  list
- * @token: format struct
- * @f: function pointer
- */
-typedef struct myToken
-{
-char *token;
-void (*f)(char *, va_list);
-} token;
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -21,3 +8,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+
