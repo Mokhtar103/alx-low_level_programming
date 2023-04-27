15de56c8f82ef98febd27d9266398f874fb3e565
Mokhtar103 <ahmedmohktar26@gmail.com>
2023-04-27 08:38:23 -0700

    prints the elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - return the length of a string
- * @str: string
- *
- * Return: the length of the string
- */
-int _strlen(char *str)
-{
-int i;
-
-i = 0;
-if (str == NULL)
-{
-return (0);
-}
-while (*str++)
-{
-i++;
-}
-return (i);
-}
-
-/**
  * print_list - prints all elements of a list
  * @h: first node
  *
@@ -34,7 +12,14 @@ size_t n = 0;
 
 while (h != NULL)
 {
-printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nill)");
+if (h->str == NULL)
+{
+printf("[0] (nill)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
 h = h->next;
 n++;
 }
