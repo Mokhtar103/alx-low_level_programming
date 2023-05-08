8fcce92df2a0bf5c3911ab256b3fe66c44401ef6
Mokhtar103 <ahmedmohktar26@gmail.com>
2023-05-08 16:31:59 -0700

    reads a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,37 +9,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *text;
-int fd, w, l;
+int fd;
+char *buff;
 
 if (filename == NULL)
 {
 return (0);
 }
 
-text = malloc(sizeof(char) * letters);
-
-if (text == NULL)
-{
-return (0);
-}
-
 fd = open(filename, O_RDONLY);
-
 if (fd == -1)
 {
 return (0);
 }
 
-l = read(fd, text, letters);
-w = write(STDOUT_FILENO, text, l);
+buff = malloc(sizeof(char) * letters);
 
-if (w == -1 || l == -1)
+letters = read(fd, buff, letters);
+
+if ((size_t)write(STDOUT_FILENO, buff, letters) != letters)
 {
 return (0);
 }
 
-free(text);
 close(fd);
-return (w);
+free(buff);
+return (letters);
 }
