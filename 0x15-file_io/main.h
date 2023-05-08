5364ca24db8397d83a150667eb41df8c9f3965e5
Mokhtar103 <ahmedmohktar26@gmail.com>
2023-05-08 16:54:48 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unsitd.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
